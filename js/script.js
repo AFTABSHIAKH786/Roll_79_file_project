e3a420054e1308427da2a90a17a66b52e1af46cc
AFTABSHIAKH786 <72864585+AFTABSHIAKH786@users.noreply.github.com>
2022-02-18 20:00:29 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function submit()
+{
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var query = document.getElementById("query").value;
+    alert(document.writeln("Name"+name));
+    alert(document.writeln("Phone "+phone));
+    alert(document.writeln("query" +query));
+}
